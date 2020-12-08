f2d4314bded7e6441541a7eff94a8a6a5f6bbf23
xmmmmmovo <13256172360@163.com>
2020-12-08 15:45:03 +0800

    添加聊天机制

--- src/plugins/chat/__init__.py
+++ src/plugins/chat/__init__.py
@@ -1,18 +1,43 @@
-from nonebot import get_driver, on_command
-from nonebot.adapters.cqhttp import Event, Bot
+from nonebot import get_driver, on_command, on_message, logger
+from nonebot.adapters.cqhttp import Event, Bot, MessageSegment, Message
 from nonebot.permission import Permission
 from nonebot.rule import to_me
 
 from .config import Config
+from .data_source import call_tencent_api
 
 global_config = get_driver().config
 config = Config(**global_config.dict())
 
-chat = on_command("", rule=to_me(), permission=Permission(), priority=99)
+chat = on_message(rule=to_me(), permission=Permission(), priority=99)
 
 
 @chat.handle()
 async def handle_first_receive(bot: Bot, event: Event, state: dict):
-    args = str(event.message).strip()  # 首次发送命令时跟随的参数，例：/天气 上海，则args为上海
-    if not args:
+    msgs = event.message
+    logger.debug(msgs)
+    str_msg = str(msgs).strip()
+    if not str_msg:
         await chat.finish("昂，怎么了吗")
+    elif str_msg == "在嘛":
+        await chat.finish("在的呢亲亲")
+    logger.debug(event.sender)
+    content = ""
+    for ms in msgs:
+        if ms.type == "text":
+            content += str(ms).strip()
+        elif ms.type == "face":
+            content += ""
+        else:
+            content += ""
+
+    resp = await call_tencent_api(event.sender["user_id"], content,
+                                  config.app_id, config.app_key)
+    if resp is None or resp["ret"] != 0:
+        await chat.finish("小扎好像听不懂呢~")
+    else:
+        smsg = Message()
+        if event.sender.get("role"):
+            smsg.append(MessageSegment.at(event.sender.get("user_id")))
+        smsg.append(resp['data']['answer'])
+        await chat.finish(smsg)
--- src/plugins/chat/config.py
+++ src/plugins/chat/config.py
@@ -3,6 +3,8 @@ from pydantic import BaseSettings
 
 class Config(BaseSettings):
     # Your Config Here
+    app_id = ""
+    app_key = ""
 
     class Config:
         extra = "ignore"
