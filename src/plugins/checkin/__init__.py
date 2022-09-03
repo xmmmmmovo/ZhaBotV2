16ae3f75b97c1e6d268ebda772b2134ada164ef4
xmmmmmovo <13256172360@163.com>
2022-09-04 00:53:59 +0800

    mod workflow

--- src/plugins/checkin/__init__.py
+++ src/plugins/checkin/__init__.py
@@ -21,7 +21,7 @@ async def handle_first_receive(matcher: Matcher, event: Event, args: Message = C
     sign_money = randint(config.sign_in_money_lower_limit,
                          config.sign_in_money_upper_limit)
     if user["has_signed"] == True:
-        await check_in.finish(MessageSegment.at(event.user_id) + "您今日已签过到，请勿重复签到！每天5:00刷新")
+        await check_in.finish(MessageSegment.at(event.user_id) + "您今日已签过到啦，就不需要再签到了，每天5:00会刷新~")
     await user_collection.update_one(*update_user_model(event.user_id, event.group_id, sign_money, True, 1))
     await group_collection.update_one(*update_inc_rank(event.group_id))
     await check_in.finish(MessageSegment.at(event.user_id) + f"签到成功！今日排名第{int(group['rank'])}名，已连续签到{int(user['days']) + 1}天！\n本次签到获得金钱：{sign_money}{config.money_unit}")
